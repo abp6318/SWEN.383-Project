11dcd7e2bb50cb6d44afd9d8940f232a839d6acb
Darlene Ardila <da7238@rit.edu>
2021-03-01 21:39:38 -0500

    Completed database.

--- src/main/resources/myPLS.sql
+++ src/main/resources/myPLS.sql
@@ -9,24 +9,22 @@ CREATE TABLE user (
   userEmail VARCHAR(100) NOT NULL,
   fName VARCHAR(75) NOT NULL,
   lName VARCHAR(75) NOT NULL,
-  accountType TINYINT (3),
+  accountType CHAR (1),
   verified BOOLEAN NOT NULL,
   userPassword VARCHAR(20) NOT NULL ,
-  PRIMARY KEY (userEmail),
-
+  PRIMARY KEY (userEmail)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE class (
-classID int AUTO_INCREMENT,
+classCode VARCHAR(20) NOT NULL,
 creatorEmail VARCHAR (100) NOT NULL, -- FK user
 professorEmail VARCHAR(100)NOT NULL, -- FK user
 className VARCHAR(100) NOT NULL,
-classCode VARCHAR(20) NOT NULL,
 learningObj TEXT,
 learningOutcome TEXT,
 beginDate DATE,
 endDate DATE,
-PRIMARY KEY (classID),
+PRIMARY KEY (classCode),
 -- creatorEmail references the userEmail in user table
 CONSTRAINT fk_creatorclass FOREIGN KEY (creatorEmail) REFERENCES user (userEmail),
 -- professorEmail references the userEmail in the user table
@@ -35,38 +33,38 @@ CONSTRAINT fk_professorclass FOREIGN KEY (professorEmail) REFERENCES user (userE
 
 CREATE TABLE classGrade (
 userEmail VARCHAR(100), -- FK user
-classID INT NOT NULL, -- FK class
+classCode VARCHAR(20), -- FK class
 grade DOUBLE,
-PRIMARY KEY (userEmail, classID),
+PRIMARY KEY (userEmail, classCode),
 -- userEmail references the userEmail in the user table
 CONSTRAINT fk_userEmailclassGrade FOREIGN KEY (userEmail) REFERENCES user (userEmail),
--- classID references the classID in the class table
-CONSTRAINT fk_classIDclassGrade FOREIGN KEY (classID) REFERENCES class (classID)
+-- classCodereferences the classCodein the class table
+CONSTRAINT fk_classCodeclassGrade FOREIGN KEY (classCode) REFERENCES class (classCode)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE classListLookup (
 userEmail VARCHAR(100), -- FK user
-classID INT, -- FK class
+classCode VARCHAR(20), -- FK class
 lectureID INT NOT NULL,
-PRIMARY KEY (userEmail, classID),
+PRIMARY KEY (userEmail, classCode),
 -- userEmail references the userEmail in the user table
 CONSTRAINT fk_userEmailclassListLookup FOREIGN KEY (userEmail) REFERENCES user (userEmail),
--- classID references the classID in the class table
-CONSTRAINT fk_classIDclassListLookup FOREIGN KEY (classID) REFERENCES class (classID)
+-- classCodereferences the classCodein the class table
+CONSTRAINT fk_classCodeclassListLookup FOREIGN KEY (classCode) REFERENCES class (classCode)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE lectures (
 lectureID INT,
-multimediaID INT, -- datatype sub until we find one for files and stuff
-PRIMARY KEY (lectureID),
+multimediaID INT NOT NULL, 
+PRIMARY KEY (lectureID, multimediaID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE lectureLookup (
-classID INT, -- FK class
-lectureID INT NOT NULL, -- FK lectures
-PRIMARY KEY (classID, userID),
--- classID references the classID in the class table
-CONSTRAINT fk_classIDlectureLookup FOREIGN KEY (classID) REFERENCES class (classID),
+classCode VARCHAR(20), -- FK class
+lectureID INT, -- FK lectures
+PRIMARY KEY (classCode, lectureID),
+-- classCodereferences the classCodein the class table
+CONSTRAINT fk_classCodelectureLookup FOREIGN KEY (classCode) REFERENCES class (classCode),
 -- lectureID references the lectureID in the lectures table
 CONSTRAINT fk_lectureIDlectureLookup FOREIGN KEY (lectureID) REFERENCES lectures (lectureID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -83,20 +81,19 @@ CONSTRAINT lectureIDlectureRating FOREIGN KEY (lectureID) REFERENCES lectures (l
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE prerequisitesLookup (
-classID INT, -- FK class
-preReqID INT, -- FK class
-PRIMARY KEY (classID),
--- classID references classID in the class table
-CONSTRAINT classIDprerequisitesLookup FOREIGN KEY (classID) REFERENCES class (classID),
+classCode VARCHAR(20), -- FK class
+preReqClassCode VARCHAR(20), -- FK class
+PRIMARY KEY (classCode, preReqClassCode),
+-- classCodereferences classCodein the class table
+CONSTRAINT classCodeprerequisitesLookup FOREIGN KEY (classCode) REFERENCES class (classCode),
 -- prereqID references prereqID in the class table
-CONSTRAINT preReqIDprerequisitesLookup FOREIGN KEY (preReqID) REFERENCES class (preReqID)
-FOREIGN KEY (preReqID) REFERENCES class (preReqID)
+CONSTRAINT preReqIDprerequisitesLookup FOREIGN KEY (preReqClassCode) REFERENCES class (classCode)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE quiz (
 quizID INT AUTO_INCREMENT,
 timeLimit DOUBLE,
-PRIMARY KEY (quizID),
+PRIMARY KEY (quizID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE quizUserScore (
@@ -112,22 +109,21 @@ CONSTRAINT userEmailquizUSerScore FOREIGN KEY (userEmail) REFERENCES user (userE
 
 CREATE TABLE classRating (
 userEmail VARCHAR(100), -- FK user
-classID INT, -- FK class
+classCode VARCHAR(20), -- FK class
 rating INT,
-PRIMARY KEY (userEmail, classID),
+PRIMARY KEY (userEmail, classCode),
 -- userEmail references the userEmail in the user table
 CONSTRAINT userEmailclassRating FOREIGN KEY (userEmail) REFERENCES user (userEmail),
--- classID references the classID in the class table
-CONSTRAINT ratingClassRating FOREIGN KEY (classID) REFERENCES class (classID)
+-- classCodereferences the classCodein the class table
+CONSTRAINT ratingClassRating FOREIGN KEY (classCode) REFERENCES class (classCode)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE quizQuestions (
 quizID INT, -- FK quiz
-quizQuestionID INT AUTO_INCREMENT,
 questionNum INT,
 questionContent TEXT,
 questionAnswer TEXT,
-PRIMARY KEY (quizID, quizQuestionID),
+PRIMARY KEY (quizID),
 -- quizID references the quizID in the quiz table
 CONSTRAINT quizIDquizQuestions  FOREIGN KEY (quizID) REFERENCES quiz(quizID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
